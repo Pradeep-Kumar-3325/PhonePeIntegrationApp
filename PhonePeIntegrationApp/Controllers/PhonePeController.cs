5d884a24e031cfc143a0501a729334491897d10f
pradeep kumar <pradeep_kumar3325@yahoo.in>
2023-10-21 14:50:19 +0530

    Fixed issue

--- PhonePeIntegrationApp/Controllers/PhonePeController.cs
+++ PhonePeIntegrationApp/Controllers/PhonePeController.cs
@@ -28,16 +28,32 @@ namespace PhonePeIntegrationApp.Controllers
             var data = new Dictionary<string, object>
             {
                 { "merchantId", _phonePeSettings.MerchentId },
-                { "merchantTransactionId", Guid.NewGuid().ToString() },
-                { "merchantUserId", Convert.ToString(order.UserId) },
-                { "merchantOrderId", order.OrderId },
-                { "amount", Convert.ToString(order.OrderAmount) },
-                { "redirectUrl", "http://localhost:4200/payment-status"},
+                { "merchantTransactionId", "MT7850590068188104" },
+                { "merchantUserId", "MUID123" },
+                { "amount", Convert.ToString(order.OrderAmount * 100) },
+                { "redirectUrl", "http://localhost:4200/"},
                 { "redirectMode", "REDIRECT"},
-                { "callbackUrl", $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}/PhonePe/Response" },
+                { "callbackUrl", "http://localhost:4200/" },
+                { "mobileNumber", "9999999999" },
                 { "paymentInstrument", new Dictionary<string, string> { { "type", "PAY_PAGE" } } }
             };
+
+            //            string payload = @"{
+            //  ""merchantId"": ""PGTESTPAYUAT"",
+            //  ""merchantTransactionId"": ""MT7850590068188104"",
+            //  ""merchantUserId"": ""MUID123"",
+            //  ""amount"": 10000,
+            //  ""redirectUrl"": ""http://localhost:4200/"",
+            //  ""redirectMode"": ""REDIRECT"",
+            //  ""callbackUrl"": ""http://localhost:4200/"",
+            //  ""mobileNumber"": ""9999999999"",
+            //  ""paymentInstrument"": {
+            //    ""type"": ""PAY_PAGE""
+            //  }
+            //}";
             var encode = Convert.ToBase64String(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data)));
+            //string k = "{\r\n  \"merchantId\": \"PGTESTPAYUAT\",\r\n  \"merchantTransactionId\": \"MT7850590068188104\",\r\n  \"merchantUserId\": \"MUID123\",\r\n  \"amount\": 10000,\r\n  \"redirectUrl\": \"http://localhost:4200/\",\r\n  \"redirectMode\": \"REDIRECT\",\r\n  \"callbackUrl\": \"http://localhost:4200/\",\r\n  \"mobileNumber\": \"9999999999\",\r\n  \"paymentInstrument\": {\r\n    \"type\": \"PAY_PAGE\"\r\n  }\r\n}";
+           // var encode = Convert.ToBase64String(Encoding.UTF8.GetBytes(k));
             var saltKey = _phonePeSettings.MerchentSecretKey;
             var saltIndex = _phonePeSettings.SaltIndex;
             var stringToHash = encode + _phonePeSettings.ApiEndpoint + saltKey;
@@ -48,9 +64,16 @@ namespace PhonePeIntegrationApp.Controllers
             using (var client = new HttpClient())
             {
                 //client.DefaultRequestHeaders.Add("Content-Type", "application/json");
-                client.DefaultRequestHeaders.Add("X-VERIFY", finalXHeader);
 
-                var requestData = new Dictionary<string, string> { { "request", encode } };
+                //- working
+                // client.DefaultRequestHeaders.Add("X-VERIFY", "d8026bc248c1ce642e67894b6e9bcef79e3996fbaf70578edbc2da7db835072f###1");
+
+               // var requestData = new Dictionary<string, string> { { "request", "ewogICJtZXJjaGFudElkIjogIlBHVEVTVFBBWVVBVCIsCiAgIm1lcmNoYW50VHJhbnNhY3Rpb25JZCI6ICJNVDc4NTA1OTAwNjgxODgxMDQiLAogICJtZXJjaGFudFVzZXJJZCI6ICJNVUlEMTIzIiwKICAiYW1vdW50IjogMTAwMDAsCiAgInJlZGlyZWN0VXJsIjogImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMC8iLAogICJyZWRpcmVjdE1vZGUiOiAiUkVESVJFQ1QiLAogICJjYWxsYmFja1VybCI6ICJodHRwOi8vbG9jYWxob3N0OjQyMDAvIiwKICAibW9iaWxlTnVtYmVyIjogIjk5OTk5OTk5OTkiLAogICJwYXltZW50SW5zdHJ1bWVudCI6IHsKICAgICJ0eXBlIjogIlBBWV9QQUdFIgogIH0KfQ==" } };
+                //- working
+                 client.DefaultRequestHeaders.Add("X-VERIFY", finalXHeader.ToLower());
+
+                 var requestData = new Dictionary<string, string> { { "request", encode } };
+
                 var content = new StringContent(JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json");
                 var response = await client.PostAsync(_phonePeSettings.PaymentApiUrl+ _phonePeSettings.ApiEndpoint, content);
                 //var responseContent = response.Content.ReadAsStringAsync().Result;
